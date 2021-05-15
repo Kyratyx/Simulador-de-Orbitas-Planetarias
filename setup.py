734435f0c7b9c1d9f0eaf2831c7e6e19bdab04d7
Kyratyx <70303578+Kyratyx@users.noreply.github.com>
2021-05-14 19:53:00 -0500

    Base

---
+++ setup.py
@@ -0,0 +1,26 @@
+'''
+Universidad Sergio Arboleda
+Autores: Juan Pablo Mora Aragón
+Fecha:Mayo 2021
+Computación Paralela y Distribuida
+Correo:Juan.mora03@correo.usa.edu.co
+'''
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+
+ext_modules = [
+    Extension('cy_simulator',
+            ['cy_simulator.pyx'],
+            libraries=['m'],
+            extra_compile_args=['-ffast-math',
+                                '-fopenmp', '-march=native'],
+            extra_link_args=['-fopenmp']
+            )]
+setup(
+    name='cy_simulator',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
+)
